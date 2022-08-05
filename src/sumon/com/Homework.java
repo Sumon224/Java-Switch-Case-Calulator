adfe81e4bb3afdfd2b887eab189185cfced04949
Sumon224 <sumon6342@gmail.com>
2022-08-06 03:27:59 +0600

    first commit

---
+++ src/sumon/com/Homework.java
@@ -0,0 +1,28 @@
+package sumon.com;
+import java.util.*;
+
+public class Homework {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+
+        int addition = a+b;
+        System.out.println(addition);
+
+        int subtraction = a-b;
+        System.out.println(subtraction);
+
+        int Multiplication = a*b;
+        System.out.println(Multiplication);
+
+        int division = a/b;
+        System.out.println(division);
+
+        int modulo = a%b;
+        System.out.println(modulo);
+
+        //I am so happy. I can do my own code in Java finally. Hard works pay off. I will be successful one day.
+    }
+}
